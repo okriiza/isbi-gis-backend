a996038909b0b0d98da495cd902c35742f1524af
okriiza <rendiokrizaputra@gmail.com>
2021-11-19 21:27:52 +0700

    update: create api

---
+++ app/Http/Controllers/API/GetDataController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Area;
+use App\Models\DetailElement;
+use App\Models\Element;
+use App\Models\Type;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class GetDataController extends Controller
+{
+    public function getArea()
+    {
+        $areas = Area::all();
+
+        if ($areas)
+            return ResponseFormatter::success($areas, 'Data Daerah berhasil diambil');
+        else
+            return ResponseFormatter::error(null, 'Data daerah tidak ada', 404);
+    }
+    public function getAreaById($id)
+    {
+        $area = Area::find($id);
+
+        if ($area)
+            return ResponseFormatter::success($area, 'Data Daerah berhasil diambil');
+        else
+            return ResponseFormatter::error(null, 'Data daerah tidak ada', 404);
+    }
+    public function getElement()
+    {
+        $elements = Element::all();
+
+        if ($elements)
+            return ResponseFormatter::success($elements, 'Data Element berhasil diambil');
+        else
+            return ResponseFormatter::error(null, 'Data Element tidak ada', 404);
+    }
+    public function getElementById($id)
+    {
+        $element = Element::find($id);
+
+        if ($element)
+            return ResponseFormatter::success($element, 'Data Element berhasil diambil');
+        else
+            return ResponseFormatter::error(null, 'Data Element tidak ada', 404);
+    }
+    public function gettype()
+    {
+        $types = Type::with('element')->get();
+
+        if ($types)
+            return ResponseFormatter::success($types, 'Data Type berhasil diambil');
+        else
+            return ResponseFormatter::error(null, 'Data Type tidak ada', 404);
+    }
+    public function gettypeById($id)
+    {
+        $type = Type::find($id);
+
+        if ($type)
+            return ResponseFormatter::success($type, 'Data Type berhasil diambil');
+        else
+            return ResponseFormatter::error(null, 'Data Type tidak ada', 404);
+    }
+    public function getDetailElementById($idElement, $idArea, $idType)
+    {
+        $getDetailElement = DetailElement::with([
+            'element' => function ($query) use ($idElement) {
+                $query->where('id', $idElement);
+            },
+            'area' => function ($query) use ($idArea) {
+                $query->where('id', $idArea);
+            },
+            'type' => function ($query) use ($idType) {
+                $query->where('id', $idType);
+            }
+        ])
+            ->where('element_id', $idElement)
+            ->where('area_id', $idArea)
+            ->where('type_id', $idType)
+            ->get();
+
+        if ($getDetailElement)
+            return ResponseFormatter::success($getDetailElement, 'Data Detail Element berhasil diambil');
+        else
+            return ResponseFormatter::error(null, 'Data Detail Element tidak ada', 404);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\GetDataController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('getDaerah', [GetDataController::class, 'getArea'])
+    ->name('getDaerah');
+Route::get('getUnsur', [GetDataController::class, 'getElement'])
+    ->name('getUnsur');
+Route::get('getJenis', [GetDataController::class, 'getType'])
+    ->name('getJenis');
+Route::get('getDetail/{idElement}/{idArea}/{idType}', [GetDataController::class, 'getDetailElementById'])
+    ->name('getDetail');
